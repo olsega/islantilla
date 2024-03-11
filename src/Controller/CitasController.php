66406c1fdd184d033af4b31e4510d15d8c19c503
olsega <oserrano@gmail.com>
2024-03-11 10:33:44 +0100

    [+] Consulta como JSON de Citas

---
+++ src/Controller/CitasController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CitasRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class CitasController extends AbstractController
+{
+    #[Route('/citas', name: 'citas_list')]
+    public function list(CitasRepository $citasRepository, Request $request): Response
+    {
+        
+        $citas = $citasRepository->findAll();
+
+        // Preparando los datos para ser devueltos en formato JSON
+        $citasArray = [];
+        foreach ($citas as $cita) {
+            $citasArray[] = [
+                'id' => $cita->getId(),
+                'id_cita' => $cita->getIdCita(),
+                'dni' => $cita->getDni()->getDni(), 
+                'id_tratamiento' => $cita->getIdTratamiento()->getNombreTratamiento(),
+                'fecha' => $cita->getFecha()->format('Y-m-d'),
+                'pagado' => $cita->isPagado(),
+                'activo' => $cita->isActivo(),
+            ];
+        }
+
+        return new JsonResponse($citasArray);
+    }
+}
